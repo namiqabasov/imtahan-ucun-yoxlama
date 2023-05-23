5e32932d5767fc593e80373376cfb29cec0f6a8b
namiqabasov <7td508c@code.edu.az>
2023-05-23 11:35:45 +0400

    yoxlama

--- yoxlama/assets/js/app.js
+++ yoxlama/assets/js/app.js
@@ -4,8 +4,8 @@ const btnright = document.querySelector(".btn-right");
 
 let a = -20;
 btnright.addEventListener("click", function () {
-  a -= 390;
-  if (a < -390) {
+  a -= 490;
+  if (a < -980) {
     a = 0;
   }
   galerylow.style.cssText = `
@@ -13,9 +13,9 @@ btnright.addEventListener("click", function () {
     `;
 });
 btnleft.addEventListener("click", function () {
-  a += 390;
+  a += 490;
   if (a > 0) {
-    a = -390;
+    a = -490;
   }
   galerylow.style.cssText = `
            left:${a}px;
@@ -25,37 +25,37 @@ btnleft.addEventListener("click", function () {
 const obj = {
   id: "children",
   img: [
-    img1: {
+    {
       img_src:
         "https://preview.colorlib.com/theme/adopted/img/children_3.jpg.webp",
       name: "Mark",
       years: "12 yrs.old",
     },
-    img2: {
+    {
       img_src:
         "https://preview.colorlib.com/theme/adopted/img/children_5.jpg.webp",
       name: "Ben",
       years: "14 yrs.old",
     },
-    img3: {
+    {
       img_src:
         "https://preview.colorlib.com/theme/adopted/img/children_4.jpg.webp",
       name: "Jessa",
       years: "13 yrs.old",
     },
-    img4: {
+    {
       img_src:
         "https://preview.colorlib.com/theme/adopted/img/children_1.jpg.webp",
       name: "Edward",
       years: "14 yrs.old",
     },
-    img5: {
+    {
       img_src:
         "https://preview.colorlib.com/theme/adopted/img/children_2.jpg.webp",
       name: "Melissa",
       years: "12 yrs.old",
     },
-    img6: {
+    {
       img_src:
         "https://preview.colorlib.com/theme/adopted/img/children_3.jpg.webp",
       name: "Mark",
@@ -63,7 +63,6 @@ const obj = {
     },
   ],
 };
-
 async function myfetch() {
   const responsedata = await fetch(
     "https://northwind.vercel.app/api/categories"
@@ -72,7 +71,9 @@ async function myfetch() {
 
   data.forEach((element) => {
     if (element.id === "children") {
-      console.log(element.img.img1);
+      element.img.forEach((e) => {
+        newcreat(e);
+      });
     }
   });
   console.log("salam");
@@ -87,5 +88,23 @@ async function myfetch() {
 //     body: JSON.stringify(obj),
 //   });
 // }
+// function myfetch(obj) {
+//   fetch("https://northwind.vercel.app/api/categories/children", {
+//     method: "DELETE",
+//   });
+// }
 
 myfetch();
+
+function newcreat(element) {
+  const mydiv = document.createElement("div");
+  const img = document.createElement("img");
+  const myp = document.createElement("p");
+  const p = document.createElement("p");
+  mydiv.classList.add("newdiv");
+  img.src = element.img_src;
+  myp.innerText = element.name;
+  p.innerText = element.years;
+  mydiv.append(img, myp, p);
+  galerylow.appendChild(mydiv);
+}
