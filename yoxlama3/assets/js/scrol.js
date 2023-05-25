eeec0dadf2daf90313aacd4240eb24d7328bd42b
namiqabasov <7td508c@code.edu.az>
2023-05-25 09:56:51 +0400

    yoxlama

---
+++ yoxlama3/assets/js/scrol.js
@@ -0,0 +1,45 @@
+ScrollReveal().reveal(".newani", {
+  delay: 600,
+  distance: "50px",
+  duraction: 600,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 100,
+    y: 100,
+    z: 100,
+  },
+});
+ScrollReveal().reveal(".animation", {
+  delay: 600,
+  distance: "50px",
+  duraction: 600,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+});
+ScrollReveal().reveal(".animation1", {
+  delay: 600,
+  distance: "50px",
+  duraction: 600,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "left",
+});
+ScrollReveal().reveal(".animation2", {
+  delay: 800,
+  distance: "50px",
+  duraction: 600,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "left",
+});
+ScrollReveal().reveal(".animation3", {
+  delay: 1000,
+  distance: "50px",
+  duraction: 600,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "left",
+});
+ScrollReveal().reveal(".animation4", {
+  delay: 1200,
+  distance: "50px",
+  duraction: 600,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "left",
+});
